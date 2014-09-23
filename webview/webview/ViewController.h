c5124557d25c6e7e61558c2e275fff8c781af0df
andyfurnival <andyfurnival@Andrews-MacBook-Pro.local>
2014-09-24 00:02:40 +0200

    fixed module

--- webview/webview/ViewController.h
+++ webview/webview/ViewController.h
@@ -1,14 +1,55 @@
 //
-//  ViewController.h
+//  ViewController.m
 //  webview
 //
 //  Created by Andrew Furnival on 20/09/2014.
 //  Copyright (c) 2014 Andrew Furnival. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <WebKit/WebKit.h>
+#import "ViewController.h"
 
-@interface ViewController : UIViewController
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    CGRect webViewBounds = self.view.bounds;
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL: [NSURL URLWithString: @"http://mobet.williamhill.es"]];
+    
+    
+    if (NSClassFromString(@"WKWebView") ) {
+#ifdef __IPHONE_8_0
+        WKUserContentController* userContentController = [[WKUserContentController alloc] init];
+        
+        
+        WKWebViewConfiguration* configuration = [[WKWebViewConfiguration alloc] init];
+        configuration.userContentController = userContentController;
+        
+        WKWebView *webview = [[WKWebView alloc] initWithFrame:webViewBounds configuration:configuration];
+        [webview loadRequest: request];
+        
+        [self.view addSubview:webview];
+#endif
+    } else {
+        UIWebView *uiwebview = [[UIWebView alloc] initWithFrame:webViewBounds];
+        [uiwebview loadRequest: request];
+        [self.view addSubview:uiwebview];
+    }
+
+
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
 @end
